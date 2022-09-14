39bd990da7915873cfe591f6b05360354d08c9a8
baiziyu <baiziyu@inspur.com>
2022-09-14 17:23:54 +0800

    Add Configuration panel.
    Change-Id: If738a21e43c8b1097829980be6e4013e14a36bf9

---
+++ venus_dashboard/enabled/_4069_admin_add_configuration_panel.py
@@ -0,0 +1,17 @@
+# The slug of the panel to be added to HORIZON_CONFIG. Required.
+PANEL = 'configuration'
+# The slug of the dashboard the PANEL associated with. Required.
+PANEL_DASHBOARD = 'admin'
+# The slug of the panel group the PANEL is associated with.
+PANEL_GROUP = 'venus'
+
+# Python panel class of the PANEL to be added.
+ADD_PANEL = 'venus_dashboard.storage_days.panel.Configuration'
+
+ADD_INSTALLED_APPS = ['venus_dashboard.configuration']
+
+ADD_ANGULAR_MODULES = ['horizon.dashboard.admin.venus']
+
+# ADD_JS_FILES = ['dashboard/admin/venus.module.js']
+
+AUTO_DISCOVER_STATIC_FILES = True
